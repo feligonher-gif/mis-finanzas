b7ef32340564e3907d616f073f5abd85d93e2b1e
feligonher-gif <feligonher@gmail.com>
2026-06-14 19:41:53 -0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'misfinanzas-v3'
+const FILES = [
+  '/',
+  '/app.html',
+  '/manifest.json',
+  '/lib/preact.js',
+  '/lib/preact-hooks.js',
+  '/lib/htm.js',
+  '/lib/chart-umd.js',
+  '/lib/xlsx.min.js'
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then(c => c.put(e.request, clone))
+        }
+        return res
+      }).catch(() => cached || new Response('Offline', {status: 503}))
+    })
+  )
+})
