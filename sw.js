8ab32de17ffceecd0151491f1d3ec38670807ff3
feligonher-gif <feligonher@gmail.com>
2026-06-16 22:44:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-const CACHE = 'misfinanzas-v5'
-const BASE = new URL('./', self.location).href
-const FILES = ['', 'index.html', 'sw.js', 'manifest.json', 'lib/preact.js', 'lib/preact-hooks.js', 'lib/htm.js', 'lib/chart-umd.js']
+const CACHE = 'misfinanzas-v6'
+const FILES = [
+  '/',
+  '/app.html',
+  '/manifest.json',
+  '/lib/preact.js',
+  '/lib/preact-hooks.js',
+  '/lib/htm.js',
+  '/lib/chart-umd.js',
+  '/lib/xlsx.min.js'
+]
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES.map(f => BASE + f))).then(() => self.skipWaiting()))
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  )
 })
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()))
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
 })
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-    const clone = res.clone()
-    caches.open(CACHE).then(c => c.put(e.request, clone))
-    return res
-  })))
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then(c => c.put(e.request, clone))
+        }
+        return res
+      }).catch(() => cached || new Response('Offline', {status: 503}))
+    })
+  )
 })
